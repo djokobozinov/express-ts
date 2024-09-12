9aacaadd7cc98d85e98c8345f7dacd4d26356738
Gjoko <djoko.bozinov@gmail.com>
2024-09-12 22:21:49 +0200

    Small refactor.

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-export const getAllUsers = async (req: Request, res: Response) => {
+export const findAll = async (req: Request, res: Response) => {
 	const { id } = req.query;
 	if (id) {
 		return res.json({ id, name: 'Gjoko Bozhinov', message: 'Not implemented! Just an example.' });
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/user.controllers';
+import { findAll } from '../controllers/user.controllers';
 
 const router = Router();
 
-router.get('/', getAllUsers);
+router.get('/', findAll);
 
 export default router;
